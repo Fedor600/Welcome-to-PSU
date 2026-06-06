852711853665edcf399978e4e69b2c6d38bb0ee7
Fedor600 <account123468@mail.ru>
2026-06-06 22:02:52 +0500

    Clear

--- Assets/Scripts/PlayerControllingAnimated.cs
+++ Assets/Scripts/PlayerControllingAnimated.cs
@@ -3,14 +3,18 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(SpriteRenderer))]
-public class PlayerControllingAnimated : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 3f;
 
     [Header("Animation")]
     [SerializeField] private float framesPerSecond = 8f;
-    [SerializeField] private Sprite idleSprite;
+    [SerializeField] private Sprite idleDown;
+    [SerializeField] private Sprite idleUp;
+    [SerializeField] private Sprite idleRight;
+    [SerializeField] private Sprite idleLeft;
+
     [SerializeField] private Sprite[] walkDown;
     [SerializeField] private Sprite[] walkUp;
     [SerializeField] private Sprite[] walkRight;
@@ -21,16 +25,26 @@ public class PlayerControllingAnimated : MonoBehaviour
     private PlayerControl playerControl;
 
     private Vector2 movement;
-    private Vector2 lastDirection = Vector2.down;
+    private Direction currentDirection = Direction.Down;
 
     private float animationTimer;
     private int frameIndex;
+
+    private enum Direction
+    {
+        Down,
+        Up,
+        Right,
+        Left
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerControl = new PlayerControl();
+
+        SetIdleSprite();
     }
 
     private void OnEnable()
@@ -52,52 +66,103 @@ public class PlayerControllingAnimated : MonoBehaviour
 
         if (movement != Vector2.zero)
         {
-            lastDirection = movement;
+            UpdateDirection();
             AnimateWalk();
         }
         else
         {
-            frameIndex = 0;
             animationTimer = 0f;
-
-            if (idleSprite != null)
-                spriteRenderer.sprite = idleSprite;
+            frameIndex = 0;
+            SetIdleSprite();
         }
     }
 
     private void FixedUpdate()
     {
+        if (movement == Vector2.zero)
+            return;
+
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void UpdateDirection()
+    {
+        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            currentDirection = movement.x > 0 ? Direction.Right : Direction.Left;
+        }
+        else
+        {
+            currentDirection = movement.y > 0 ? Direction.Up : Direction.Down;
+        }
     }
 
     private void AnimateWalk()
     {
-        Sprite[] currentAnimation = GetCurrentAnimation();
+        Sprite[] animation = GetWalkAnimation();
 
-        if (currentAnimation == null || currentAnimation.Length == 0)
+        if (animation == null || animation.Length == 0)
             return;
 
         animationTimer += Time.deltaTime;
 
         if (animationTimer >= 1f / framesPerSecond)
         {
-            animationTimer = 0f;
-            frameIndex++;
-
-            if (frameIndex >= currentAnimation.Length)
-                frameIndex = 0;
+            animationTimer -= 1f / framesPerSecond;
+            frameIndex = (frameIndex + 1) % animation.Length;
         }
 
-        spriteRenderer.sprite = currentAnimation[frameIndex];
+        spriteRenderer.flipX = false;
+        spriteRenderer.sprite = animation[frameIndex];
     }
 
-    private Sprite[] GetCurrentAnimation()
+    private Sprite[] GetWalkAnimation()
     {
-        if (Mathf.Abs(lastDirection.x) > Mathf.Abs(lastDirection.y))
+        switch (currentDirection)
         {
-            return lastDirection.x > 0 ? walkRight : walkLeft;
+            case Direction.Up:
+                return walkUp;
+            case Direction.Down:
+                return walkDown;
+            case Direction.Right:
+                return walkRight;
+            case Direction.Left:
+                return walkLeft;
+            default:
+                return walkDown;
         }
+    }
 
-        return lastDirection.y > 0 ? walkUp : walkDown;
+    private void SetIdleSprite()
+    {
+        spriteRenderer.flipX = false;
+
+        switch (currentDirection)
+        {
+            case Direction.Up:
+                if (idleUp != null) spriteRenderer.sprite = idleUp;
+                break;
+
+            case Direction.Down:
+                if (idleDown != null) spriteRenderer.sprite = idleDown;
+                break;
+
+            case Direction.Right:
+                if (idleRight != null) spriteRenderer.sprite = idleRight;
+                break;
+
+            case Direction.Left:
+                if (idleLeft != null) spriteRenderer.sprite = idleLeft;
+                break;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (framesPerSecond <= 0f)
+            framesPerSecond = 1f;
+
+        if (moveSpeed < 0f)
+            moveSpeed = 0f;
     }
 }
